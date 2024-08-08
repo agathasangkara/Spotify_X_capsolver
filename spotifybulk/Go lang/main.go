c2ed074098e105f9e0660950e30dd9ca2b2bd6f8
zuand000 <134229492+zuand000@users.noreply.github.com>
2024-08-08 09:15:24 +0700

    Update main.go

--- spotifybulk/Go lang/main.go
+++ spotifybulk/Go lang/main.go
@@ -2,18 +2,107 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"io/ioutil"
-	"math/rand"
+	"io"
 	"net/http"
-	"os"
-	"strconv"
-	"strings"
-
-	"github.com/fatih/color"
+	"time"
 )
 
+type capSolverResponse struct {
+	ErrorId          int32          `json:"errorId"`
+	ErrorCode        string         `json:"errorCode"`
+	ErrorDescription string         `json:"errorDescription"`
+	TaskId           string         `json:"taskId"`
+	Status           string         `json:"status"`
+	Solution         map[string]any `json:"solution"`
+}
+
+func capSolver(ctx context.Context, apiKey string, taskData map[string]any) (*capSolverResponse, error) {
+	uri := "https://api.capsolver.com/createTask"
+	res, err := request(ctx, uri, map[string]any{
+		"clientKey": apiKey,
+		"task":      taskData,
+	})
+	if err != nil {
+		return nil, err
+	}
+	if res.ErrorId == 1 {
+		return nil, errors.New(res.ErrorDescription)
+	}
+
+	uri = "https://api.capsolver.com/getTaskResult"
+	for {
+		select {
+		case <-ctx.Done():
+			return res, errors.New("solve timeout")
+		case <-time.After(time.Second):
+			break
+		}
+		res, err = request(ctx, uri, map[string]any{
+			"clientKey": apiKey,
+			"taskId":    res.TaskId,
+		})
+		if err != nil {
+			return nil, err
+		}
+		if res.ErrorId == 1 {
+			return nil, errors.New(res.ErrorDescription)
+		}
+		if res.Status == "ready" {
+			return res, err
+		}
+	}
+}
+
+func request(ctx context.Context, uri string, payload interface{}) (*capSolverResponse, error) {
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", uri, bytes.NewReader(payloadBytes))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	capResponse := &capSolverResponse{}
+	err = json.Unmarshal(responseData, capResponse)
+	if err != nil {
+		return nil, err
+	}
+	return capResponse, nil
+}
+
+func main() {
+	apikey := "CAP-5D0AF3E2F4B177CFDC3AEAC80163DE0A"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
+	defer cancel()
+
+	res, err := capSolver(ctx, apikey, map[string]any{
+		"type":       "ReCaptchaV2TaskProxyLess",
+		"websiteURL": "https://www.google.com/recaptcha/api2/demo",
+		"websiteKey": "6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res.Solution["gRecaptchaResponse"])
+}
+```
+
+        
 type SpotifyAPI struct {
 	Client   *http.Client
 	Proxies  []string
